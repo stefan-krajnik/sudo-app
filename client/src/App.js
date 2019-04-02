7d8da2ee39b34501606ecfe91e50653c485a1bc9
stefankrajnik <stefankrajnik1@gmail.com>
2019-04-02 19:06:34 +0200

    Add Test string

--- client/src/App.js
+++ client/src/App.js
@@ -23,6 +23,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
+          <p>Test</p>
           <p>
             {this.state.message}
           </p>
